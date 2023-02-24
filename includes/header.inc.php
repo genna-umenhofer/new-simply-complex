4ad56d497bf1e962a7aa4847455a5f832892de8c
Genna Umenhofer <gumenhofer@gmail.com>
2023-02-24 06:31:58 -0600

    SEO updates

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -26,6 +26,7 @@ if(basename($_SERVER['PHP_SELF']) == "index.php") {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Buy ethically sourced crystals, mini llamas, and handmade clothing from a local midwest art shop. Visit the event page to find us in person!">
   <link rel="stylesheet" href="<?php echo $style?>">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
   <link rel="icon" href="<?php echo $logo?>">
--- pages/events.php
+++ pages/events.php
@@ -1,10 +1,14 @@
 <?php 
 include("../includes/header.inc.php");
 ?>
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v16.0" nonce="9DsX08vr"></script>
+
 <div class="events-container">
   <h1 class="top-heading">Upcoming Live Events</h1>
-  <hr>
+  <hr><br>
 </div>
+<div class="event-and-social-container">
 <div class="events-card-container">
 
   <div class="events-card">
@@ -39,5 +43,13 @@ include("../includes/header.inc.php");
   </div>
 
 </div>
+<div class="social-feed">
+  <div class="facebook">
+  <div class="fb-page" data-href="https://www.facebook.com/SimplyComplexArtShop" data-tabs="timeline" data-width="340" data-height="600" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/SimplyComplexArtShop" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/SimplyComplexArtShop">Simply Complex Art Shop</a></blockquote></div>
+  </div>
+  <div class="insta"></div>
+  <div class="youtube"></div>
 
+</div>
+</div>
 <?php include("../includes/footer.inc.php");?>
