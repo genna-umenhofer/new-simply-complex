85670ea30ef14e077f44d742e5af50eaa1b39776
Genna Umenhofer <gumenhofer@gmail.com>
2023-02-26 17:38:25 -0600

    added store coming soon page

--- pages/shop.php
+++ pages/shop.php
@@ -1,6 +1,10 @@
 <?php 
 include("../includes/header.inc.php");
 ?>
-
+<div class="shop-container">
+  <h1 class="top-heading">Our online store is coming soon!</h1>
+  <h3 class="third-heading">Can't wait?</h3>
+  <a href="https://www.etsy.com/shop/SimplyComplexArtShop" class="button-light" target="_blank">Visit Us on Etsy!</a>
+</div>
 
 <?php include("../includes/footer.inc.php");?>
