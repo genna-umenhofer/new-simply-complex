9bff3c16c990b79a1ab5907da9dafa51d292f414
Genna Umenhofer <gumenhofer@gmail.com>
2023-02-26 18:18:55 -0600

    added php mailer

--- includes/sendemail.php
+++ includes/sendemail.php
@@ -1,38 +1,52 @@
 <?php
 
-$name = $_POST["Name"];
-$email = $_POST["Email"];
-$subject = $_POST["Subject"];
-$message = $_POST["Message"];
 
-require "vendor/autoload.php";
+
+require "../vendor/autoload.php";
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 
-$mail = new PHPMailer(true);
+// Include the recaptcha autoload file
+//require_once __DIR__ . '../vendor/autoload.php';
 
-//$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+// Verify the user's response token
+$recaptcha = new \ReCaptcha\ReCaptcha('6LfreYokAAAAAGXqPesaxeZSkTT1b5RpxYQBaLK0');
+$resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
 
-$mail->isSMTP();
-$mail->SMTPAuth = true;
+if ($resp->isSuccess()) {
+    // The user's response token is valid
+    //echo '<script>alert("It worked!")</script>';
+    $name = $_POST["Name"];
+    $email = $_POST["Email"];
+    $subject = $_POST["Subject"];
+    $message = $_POST["Message"];
 
-$mail->Host = "smtp.gmail.com";
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
+    $mail = new PHPMailer(true);
 
-$mail->Username = "genna.umenhofer.developer@gmail.com";
-$mail->Password = "fnszcgqapmjofxfz";
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
-$mail->setFrom($email, $name);
-$mail->addAddress("genna.umenhofer.developer@gmail.com", "Genna");
+    $mail->isSMTP();
+    $mail->SMTPAuth = true;
 
-$mail->Subject = "New messge from portfolio site";
-$mail->Body = $message;
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
 
-$mail->send();
+    $mail->Username = "genna.umenhofer.developer@gmail.com";
+    $mail->Password = "fnszcgqapmjofxfz";
 
-header("Location: ../pages/sent.php");
+    $mail->setFrom($email, $name);
+    $mail->addAddress("genna.umenhofer.developer@gmail.com", "Genna");
 
+    $mail->Subject = "New messge from Simply Complex Art Shop";
+    $mail->Body = $name . $subject . $message . $email;
 
-?>
+    $mail->send();
+
+    header("Location: ../pages/sent.php");
+} else {
+    // The user's response token is invalid
+    echo '<script>alert("Something went wrong. Please go back and try again.")</script>';
+    $errors = $resp->getErrorCodes();
+}
